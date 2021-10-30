69e05d5adb5517d6db0a8ccf3e53bd42237278a5
Ali Abana <neroabana@gmail.com>
2021-10-30 09:59:24 +0100

    mobileHeader

--- src/MobileHeader.js
+++ src/MobileHeader.js
@@ -4,7 +4,6 @@ import mobile_sort_icon from "./files/icon_arrow01.svg";
 import { contextSort } from "./Context";
 
 const Header = () => {
-  
   let [headerLabelChoice, setHeaderLabelChoice] = useContext(contextSort);
   
   return (
